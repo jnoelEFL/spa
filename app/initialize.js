f19a75202f69dd33504ebaeffb62d64ff86d97b2
= <=>
2014-05-23 17:31:12 +0200

    formatage et pushState
    ajout du setting de l'api pour le formatage monetaire + suppression de
    la fonction pushState sinon ca plante en production

--- app/initialize.js
+++ app/initialize.js
@@ -2,9 +2,25 @@ var Application = require('application');
 var routes = require('routes');
 
 $(function() {
+  accounting.settings = {
+  currency: {
+    symbol : '€',   // default currency symbol is '$'
+    format: '%v', // controls output: %s = symbol, %v = value/number (can be object: see below)
+    decimal : '.',  // decimal point separator
+    thousand: ' ',  // thousands separator
+    precision : 0   // decimal places
+  },
+  number: {
+    precision : 0,  // default precision on numbers is 0
+    thousand: ' ',
+    decimal : '.'
+  }
+};
+
   return new Application({
     title: 'Simulateur example application',
     controllerSuffix: '-controller',
-    routes: routes
+    routes: routes,
+    pushState: false
   });
 });
