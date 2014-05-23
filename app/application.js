fca2aae3c70e77da0c5b9cb4f9e1ce5612d4b18d
= <=>
2014-05-23 17:26:10 +0200

    suppression des commentaires pour appel du super constructeur

--- app/application.js
+++ app/application.js
@@ -1,8 +1,9 @@
 // The application object.
 module.exports = Application = Chaplin.Application.extend({
-  // start: function() {
-  //   // You can fetch some data here and start app
-  //   // (by calling `super`) after that.
-  //   this.constructor.__super__.start.call(this);
-  // }
-})
+  start: function() {
+    // You can fetch some data here and start app
+    // (by calling `super`) after that.
+    //this.data = new Data();
+    this.constructor.__super__.start.call(this);
+  }
+});
